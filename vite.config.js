816e3e8785e0585eb8d9d135792c0a386624e12b
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-04-18 20:19:08 +0530

    comment added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: { //added to fix the issue of reloading on routes
     historyApiFallback: true,
   }
 
